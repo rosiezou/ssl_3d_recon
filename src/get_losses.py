119968046447a0b13f7a4fb25df6ce2bf863de61
rosiezou <rosiezou@gmail.com>
2021-03-15 07:00:18 +0000

    added new loss function, new data generation script & new notebook to viz blobs :)

--- src/get_losses.py
+++ src/get_losses.py
@@ -66,6 +66,134 @@ def get_img_loss(gt, pred, mode='l2_sq', affinity_loss=False):
     return loss, min_dist, min_dist_inv
 
 
+## helper functions for k-random-octant loss
+def compute_l2_norm(pcl_mat, octants):
+    # pcl_mat: BATCH_SIZE x 1024 x 3 tensor
+    # octants: BATCH_SIZE k x 3 tensor
+    # output: BATCH_SIZE x 1024 x k matrix where each row contains l2 norm of each point against each of the k centers.
+    
+    ## this is inefficient for larger batches
+    num_points = pcl_mat.shape[1]
+    num_octants = octants.shape[1]
+    distance = np.zeros((num_points, num_octants))
+    num_batches = pcl_mat.shape[0]
+    outputTensor = tf.zeros(shape = (1, pcl_mat.shape[1], octants.shape[1]))
+    for batch in range(num_batches):
+        output_batch = tf.zeros(shape = (1, pcl_mat.shape[1], 0))
+        for octant in range(num_octants):
+            # Tensor("ExpandDims_62:0", shape=(1, 1, 3), dtype=float32)
+            # Tensor("sub_60:0", shape=(1, 1024, 3), dtype=float32)
+            # Tensor("norm/Sqrt:0", shape=(1, 1024, 1), dtype=float32)
+            single_octant_center = tf.expand_dims(
+                tf.expand_dims(octants[batch, octant, :], 
+                    axis = 0), axis = 0)
+            diff = pcl_mat[batch, :, :] - single_octant_center
+            norm_for_all_points = tf.norm(diff, axis = 2, keepdims = True)
+            output_batch = tf.concat([output_batch, norm_for_all_points], axis = 2)
+        outputTensor = tf.concat([outputTensor, output_batch], axis = 0)
+    outputTensor = outputTensor[1:, :, :]
+    return outputTensor
+
+def find_min_distance_to_cluster(distance, pcl):
+    # distance: 1024 x k matrix where each row contains l2 norm of each point against each of the k centers.
+    # pcl: 1024 x 3 point clouds
+    # output: Tuple of a 1xk matrix contains the row index of the min entry and another
+    # 1 x k matrix where each entry contains the min_distance from a given pointcloud point to the
+    # kth cluster.
+    min_distance = tf.reduce_min(distance, axis=1, keepdims=True)
+    min_distance_row_index = tf.argmin(distance, axis = 1)
+    
+    ## gather points for first batch
+    anchor_points = tf.gather(pcl[0, :, :], min_distance_row_index[0, :])
+    anchor_points = tf.expand_dims(anchor_points, axis = 0)
+
+    ## stack the rest
+    for i in range(1, pcl.shape[0]):
+        anchor_points = tf.concat([anchor_points, tf.expand_dims(tf.gather(pcl[i, :, :], min_distance_row_index[i, :]), axis = 0)], axis = 0)
+
+    return anchor_points, min_distance_row_index
+
+## TODO: add checks for octants not containing any anchor points
+##       should those be omitted? or still summed up but then we
+##       add the rigidity score as a regularization term?
+
+def getAnchorPoints(pcl_tensor, k):
+    '''
+    pcl_tensor: tensor of dim = (BS,N_pts,3)
+    '''
+    min_vals = tf.reduce_min(pcl_tensor, axis = 1)
+    max_vals = tf.reduce_max(pcl_tensor, axis = 1)
+    intervals = (max_vals - min_vals)/k
+    
+    ## this works but it's inefficient for large values of k
+    batchArray = []
+    for batch in range(pcl_tensor.shape[0]):
+        x_min, y_min, z_min = min_vals[batch, 0], min_vals[batch, 1], min_vals[batch, 2]
+        x_max, y_max, z_max = max_vals[batch, 0], max_vals[batch, 1], max_vals[batch, 2]
+        x_interval, y_interval, z_interval = intervals[batch, 0], intervals[batch, 1], intervals[batch, 2]
+        ## compute geometric centers
+        searchVolumes = []
+        overallIndex = 1
+        x_init, y_init, z_init = x_min - x_interval/2, y_min - y_interval/2, z_min - z_interval/2
+        for p in range(0, k):
+            x_init += x_interval
+            for q in range(0, k):
+                y_init += y_interval
+                for r in range(0, k):
+                    z_init += z_interval
+                    searchVolumes.append([x_init, y_init, z_init])
+                    overallIndex += 1
+                z_init = z_min - z_interval/2
+            y_init = y_min - y_interval/2
+                
+        batchArray.append(searchVolumes)
+    octants = tf.convert_to_tensor(batchArray)
+
+    ## get anchor points' distances
+    ## L2_norm([1024, 3], [27, 3]) ==> [1024, 27] all point clouds dist to all geometric centers ==> [1, 27]
+    distance = compute_l2_norm(pcl_tensor, octants)
+    return find_min_distance_to_cluster(distance, pcl_tensor)
+
+def getCenterPoint(pcl_tensor):
+    min_vals = tf.reduce_min(pcl_tensor, axis = 1)
+    max_vals = tf.reduce_max(pcl_tensor, axis = 1)
+    geometric_center = tf.expand_dims(min_vals + (max_vals - min_vals)/2, axis = 1)
+    l2_norms = compute_l2_norm(pcl_tensor, geometric_center)
+    return find_min_distance_to_cluster(l2_norms, pcl_tensor)
+
+def get_k_random_octant_loss(gt, pred, min_k, max_k):
+    '''
+    K random octant loss
+    Args:
+        gt: (BS,N_pts,3); GT point cloud
+        pred: (BS,N_pts,3); predicted point cloud
+        min_k: min value for uniformly sampled k
+        max_k: max value for uniformly sampled k
+    Returns:
+        loss: (); averaged chamfer/emd loss
+    Note:
+        k = random number to divide each axis
+        total # of octants = k^3
+    '''
+    ## fetch all anchor points
+    k = int(round(np.random.uniform(min_k, max_k)))
+    gt_anchor_points, _ = getAnchorPoints(gt, k)
+    pred_anchor_points, _ = getAnchorPoints(pred, k)
+
+    ## fetch the center points
+    gt_center_point, _= getCenterPoint(gt)
+    pred_center_point, _ = getCenterPoint(pred)
+
+    ## calculate sum of Euclidean distance between anchor points and center points, for both gt and pred
+    gt_anchor_to_center_dist = tf.norm(gt_anchor_points - gt_center_point, axis = 2)
+    pred_anchor_to_center_dist = tf.norm(pred_anchor_points - pred_center_point, axis = 2)
+
+    ## return difference of the two sums of Euclidean distance
+    ##    currently all octants are considered, regardless if they're empty
+    loss = tf.reduce_sum(gt_anchor_to_center_dist, axis = 1) - tf.reduce_sum(pred_anchor_to_center_dist, axis = 1)
+    loss = tf.reduce_mean(loss, axis = 0)
+    return loss
+
 def get_2d_symm_loss(img):
     '''
     Symmetry loss: to enforce symmetry in image along vertical axis in the
---
+++ src/utils/generate_pointcloud_gt.py
@@ -0,0 +1,225 @@
+# Custom script to generate pointcloud data.
+
+import numpy as np
+import math
+import os
+from itertools import accumulate as _accumulate, repeat as _repeat
+from bisect import bisect as _bisect
+import random
+
+# Code in this script is derived from:
+# 1. https://towardsdatascience.com/deep-learning-on-point-clouds-implementing-pointnet-in-google-colab-1fd65cd3a263
+# 2. https://github.com/davidstutz/bpy-visualization-utils
+# Note this script must be run with python>=3.5
+
+
+SHAPENET_DATA_DIR = '/data'
+PCL_DATA_DIR = '../../data/ShapeNet_v1/'
+GT_POINTCLOUD_NAME = 'gt_pointcloud_1024.npy'
+INPUT_OBJ_FILE_NAME = 'model.obj'
+INTERMEDIATE_OFF_FILE_NAME = 'model.off'
+
+categs = ['02958343']
+# categs = ['02691156', '02958343', '03001627']
+
+# Python 3.5 doesn't support random.choices.
+# This function is meant to be a replacement for the same.
+def choices(population, weights=None, cum_weights=None, k=1):
+    """Return a k sized list of population elements chosen with replacement.
+    If the relative weights or cumulative weights are not specified,
+    the selections are made with equal probability.
+    """
+    n = len(population)
+    if cum_weights is None:
+        if weights is None:
+            _int = int
+            n += 0.0    # convert to float for a small speed improvement
+            return [population[_int(random.random() * n)] for i in _repeat(None, k)]
+        cum_weights = list(_accumulate(weights))
+    elif weights is not None:
+        raise TypeError('Cannot specify both weights and cumulative weights')
+    if len(cum_weights) != n:
+        raise ValueError('The number of weights does not match the population')
+    bisect = _bisect
+    total = cum_weights[-1] + 0.0   # convert to float
+    hi = n - 1
+    return [population[bisect(cum_weights, random.random() * total, 0, hi)]
+            for i in _repeat(None, k)]
+
+
+def write_off(file, vertices, faces):
+    """
+    Writes the given vertices and faces to OFF.
+    :param vertices: vertices as tuples of (x, y, z) coordinates
+    :type vertices: [(float)]
+    :param faces: faces as tuples of (num_vertices, vertex_id_1, vertex_id_2, ...)
+    :type faces: [(int)]
+    """
+
+    num_vertices = len(vertices)
+    num_faces = len(faces)
+    assert num_vertices > 0
+    assert num_faces > 0
+    with open(file, 'w+') as fp:
+        fp.write('OFF\n')
+        fp.write(str(num_vertices) + ' ' + str(num_faces) + ' 0\n')
+        for vertex in vertices:
+            assert len(vertex) == 3, 'invalid vertex with %d dimensions found (%s)' % (len(vertex), file)
+            fp.write(str(vertex[0]) + ' ' + str(vertex[1]) + ' ' + str(vertex[2]) + '\n')
+        for face in faces:
+            assert face[0] == 3, 'only triangular faces supported (%s)' % file
+            assert len(face) == 4, 'faces need to have 3 vertices, but found %d (%s)' % (len(face), file)
+            for i in range(len(face)):
+                assert face[i] >= 0 and face[i] < num_vertices, 'invalid vertex index %d (of %d vertices) (%s)' % (face[i], num_vertices, file)
+                fp.write(str(face[i]))
+                if i < len(face) - 1:
+                    fp.write(' ')
+            fp.write('\n')
+        # add empty line to be sure
+        fp.write('\n')
+
+
+def read_obj(file):
+    """
+    Reads vertices and faces from an obj file.
+    :param file: path to file to read
+    :type file: str
+    :return: vertices and faces as lists of tuples
+    :rtype: [(float)], [(int)]
+    """
+
+    assert os.path.exists(file), 'file %s not found' % file
+    with open(file, 'r') as fp:
+        lines = fp.readlines()
+        lines = [line.strip() for line in lines if line.strip()]
+        vertices = []
+        faces = []
+        for line in lines:
+            parts = line.split(' ')
+            parts = [part.strip() for part in parts if part]
+            if parts[0] == 'v':
+                assert len(parts) == 4, \
+                    'vertex should be of the form v x y z, but found %d parts instead (%s)' % (len(parts), file)
+                assert parts[1] != '', 'vertex x coordinate is empty (%s)' % file
+                assert parts[2] != '', 'vertex y coordinate is empty (%s)' % file
+                assert parts[3] != '', 'vertex z coordinate is empty (%s)' % file
+                vertices.append([float(parts[1]), float(parts[2]), float(parts[3])])
+            elif parts[0] == 'f':
+                assert len(parts) == 4, \
+                    'face should be of the form f v1/vt1/vn1 v2/vt2/vn2 v2/vt2/vn2, but found %d parts (%s) instead (%s)' % (len(parts), line, file)
+                components = parts[1].split('/')
+                assert len(components) >= 1 and len(components) <= 3, \
+                   'face component should have the forms v, v/vt or v/vt/vn, but found %d components instead (%s)' % (len(components), file)
+                assert components[0].strip() != '', \
+                    'face component is empty (%s)' % file
+                v1 = int(components[0])
+                components = parts[2].split('/')
+                assert len(components) >= 1 and len(components) <= 3, \
+                    'face component should have the forms v, v/vt or v/vt/vn, but found %d components instead (%s)' % (len(components), file)
+                assert components[0].strip() != '', \
+                    'face component is empty (%s)' % file
+                v2 = int(components[0])
+                components = parts[3].split('/')
+                assert len(components) >= 1 and len(components) <= 3, \
+                    'face component should have the forms v, v/vt or v/vt/vn, but found %d components instead (%s)' % (len(components), file)
+                assert components[0].strip() != '', \
+                    'face component is empty (%s)' % file
+                v3 = int(components[0])
+                #assert v1 != v2 and v2 != v3 and v3 != v2, 'degenerate face detected: %d %d %d (%s)' % (v1, v2, v3, file)
+                if v1 == v2 or v2 == v3 or v1 == v3:
+                    # print('[Info] skipping degenerate face in %s' % file)
+                    continue
+                else:
+                    faces.append([v1 - 1, v2 - 1, v3 - 1]) # indices are 1-based!
+            else:
+              # continue to next line until one of the required args is found.
+              continue
+        return np.array(vertices, dtype=float), np.array(faces, dtype=int)
+    assert False, 'could not open %s' % file
+
+
+# Convert obj to an off file and save locally
+def convert_obj_to_off(obj_file, output_filename):
+  if not os.path.exists(obj_file):
+    print(obj_file, ' does not exist.')
+    return
+  obj_vertices, obj_faces = read_obj(obj_file)
+  assert obj_vertices.shape[1] == 3
+  assert obj_faces.shape[1] == 3
+  temp_faces = np.ones((obj_faces.shape[0], 4), dtype = int)*3
+  # print(obj_faces.shape)
+  temp_faces[:, 1:4] = obj_faces[:, :]
+  write_off(output_filename, obj_vertices.tolist(), temp_faces.tolist())
+
+
+def read_cad_off(filename):
+  # Read CAD files with .off extension.
+  with open(filename, 'r') as f:
+    if 'OFF' != f.readline().strip():
+        print('Error: ', filename, ' is a not a file.')
+        return false
+    num_vertices, num_faces, _ = tuple([int(s) for s in f.readline().strip().split(' ')])
+    vertices = [[float(s) for s in f.readline().strip().split(' ')] for ith_vertex in range(num_vertices)]
+    faces = [[int(s) for s in f.readline().strip().split(' ')][1:] for ith_face in range(num_faces)]
+    return vertices, faces
+
+
+def calculate_triangle_area(pt1, pt2, pt3):
+  # Calculate the area of the triangle formed by the provided points.
+  # Note: This area calculation is for a 3D triangle with Heron's formula
+  side_a = np.linalg.norm(pt1 - pt2)
+  side_b = np.linalg.norm(pt2 - pt3)
+  side_c = np.linalg.norm(pt3 - pt1)
+  perimeter = 0.5 * ( side_a + side_b + side_c)
+  return max(perimeter * (perimeter - side_a) * (perimeter - side_b) * (perimeter - side_c), 0)**0.5
+
+
+# Sample points on the surface of the chosen triangle
+def sample_point_on_triangle(pt1, pt2, pt3):
+    # barycentric coordinates on a triangle
+    # https://mathworld.wolfram.com/BarycentricCoordinates.html
+    # Another good reference: https://pharr.org/matt/blog/2019/02/27/triangle-sampling-1.html
+    s, t = sorted([random.random(), random.random()])
+    f = lambda i: s * pt1[i] + (t-s) * pt2[i] + (1-t) * pt3[i]
+    return (f(0), f(1), f(2))
+
+
+if __name__ == '__main__':
+    for category in categs:
+        cat_pcl_data_dir = os.path.join(PCL_DATA_DIR, category)
+        print('Category: ', category)
+        iteration = 0
+        for npy_dir in os.listdir(cat_pcl_data_dir):
+            iteration+=1
+            output_base_dir = os.path.join(cat_pcl_data_dir, npy_dir)
+            print('Num: ', iteration)
+            if not output_base_dir:
+                continue
+            # If the output .npy file exists, continue to the next file.
+            output_data_path = os.path.join(output_base_dir, GT_POINTCLOUD_NAME)
+            # if os.path.isfile(output_data_path):
+            #     continue
+            input_filepath = os.path.join(SHAPENET_DATA_DIR, category, npy_dir, INPUT_OBJ_FILE_NAME)
+            intermediate_filepath = os.path.join(SHAPENET_DATA_DIR, category, npy_dir, INTERMEDIATE_OFF_FILE_NAME)
+            # Convert .obj file to .off file
+            print('Converting ', input_filepath, ' to intermediary file: ', intermediate_filepath)
+            convert_obj_to_off(input_filepath, intermediate_filepath)
+            off_vertices, off_faces = read_cad_off(intermediate_filepath)
+            # Calculate area for each face
+            areas = np.zeros((len(off_faces)))
+            off_vertices = np.array(off_vertices)
+            for i in range(len(areas)):
+                areas[i] = calculate_triangle_area(off_vertices[off_faces[i][0]], off_vertices[off_faces[i][1]], off_vertices[off_faces[i][2]])
+            k = 1024
+            sampled_faces = choices(off_faces, weights=areas, k=k)
+            # Generate pointcloud.
+            print('Generating pointcloud at: ', output_data_path)
+            pointcloud = np.zeros((k, 3))
+            for i in range(len(sampled_faces)):
+                pointcloud[i] = (sample_point_on_triangle(off_vertices[sampled_faces[i][0]], off_vertices[sampled_faces[i][1]], off_vertices[sampled_faces[i][2]]))
+            np.save(output_data_path, pointcloud, allow_pickle=True)
+
+
+
+
+
